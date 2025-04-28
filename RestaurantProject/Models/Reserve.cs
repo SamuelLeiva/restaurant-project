775da54dd3ea971420ca6a12cf0efbfe72fa6c3e
SamuelLeiva <leiva20152@gmail.com>
2025-04-28 10:26:57 -0500

    Clase Reserve modelada

--- RestaurantProject/Models/Reserve.cs
+++ RestaurantProject/Models/Reserve.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RestaurantProject.Models.Abstracts;
 
 namespace RestaurantProject.Models;
 
-public class Client : Person
+public class Reserve
 {
     private static int _nextId = 1;
 
     public int Id { get; private set; }
-    //Lista de reservas
-    public List<Reserve> Reserves { get; set; }
+    public DateTime DateAndHour { get; set; }
+    public Client Client { get; set; }
+    public Table Table { get; set; }
 
-    public Client()
+    public Reserve() 
     {
-        Reserves = new List<Reserve>();
         Id = _nextId++;
     }
 }
