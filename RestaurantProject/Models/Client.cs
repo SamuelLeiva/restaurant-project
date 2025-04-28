bcf1eee6ec4676f29db773747ba74347954a5f9e
SamuelLeiva <leiva20152@gmail.com>
2025-04-28 10:19:51 -0500

    Clase cliente modelada

---
+++ RestaurantProject/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantProject.Models.Abstracts;
+
+namespace RestaurantProject.Models;
+
+public class Client : Person
+{
+    //Lista de reservas
+    List<Reserve> Reserves { get; set; }
+
+    public Client()
+    {
+        Reserves = new List<Reserves>();
+    }
+}
