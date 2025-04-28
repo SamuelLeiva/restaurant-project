149a6047199f81e8c221fa83026eee289ec2274a
SamuelLeiva <leiva20152@gmail.com>
2025-04-28 12:57:52 -0500

    Restaurant system base

---
+++ RestaurantProject/RestaurantSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProject;
+
+public class RestaurantSystem
+{
+
+    //listas de mesas, clientes y reservas iniciales
+
+
+    static void ClientManagement()
+    {
+
+    }
+
+    static void TableManagement()
+    {
+    }
+
+    static void ReserveManagement()
+    {
+    }
+
+    static void FillData()
+    {
+
+    }
+}
+
+
