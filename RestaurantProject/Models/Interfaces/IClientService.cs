b4c1d0c9ec721906dc2f566826ef49e5b52d3358
SamuelLeiva <leiva20152@gmail.com>
2025-04-28 12:40:48 -0500

    Interfaces de servicios creados

---
+++ RestaurantProject/Models/Interfaces/IClientService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProject.Models.Interfaces;
+
+public interface IClientService
+{
+    void GetAllClients();
+    Client GetClientById(int id);
+    void CreateClient();
+}
---
+++ RestaurantProject/Models/Interfaces/IReserveService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProject.Models.Interfaces;
+
+public interface IReserveService
+{
+    void GetAllReserves();
+    void CreateReserve();
+
+    //Get reserves by client
+    void GetReservesByClient(int clientId);
+
+    //Get reserves by table
+    void GetReservesByTable(int tableId);
+
+    //Get reserves by date
+    void GetReserveByDate(DateTime date);
+}
