f891b8fb9f26499861ee286a98a639d9c0c9f27c
SamuelLeiva <leiva20152@gmail.com>
2025-04-28 10:03:12 -0500

    Clase Table modelada

---
+++ RestaurantProject/Modelos/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProject.Modelos;
+
+public class Table
+{
+    private static int _nextId = 1;
+
+    public int Id { get; private set; }
+    public int NumSeats { get; set; }
+
+    public Table()
+    {
+        Id = _nextId++;
+    }
+}
