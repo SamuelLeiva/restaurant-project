ec64e0311a8a71e1373f81b73a35813464f7df0a
SamuelLeiva <leiva20152@gmail.com>
2025-04-28 10:06:29 -0500

    Enum Genre modelado

---
+++ RestaurantProject/Models/Enums/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProject.Models.Enums;
+
+public enum Genre
+{
+    MASCULINO,
+    FEMENINO
+}
--- RestaurantProject/Models/Table.cs
+++ RestaurantProject/Models/Table.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RestaurantProject.Modelos;
+namespace RestaurantProject.Models;
 
 public class Table
 {
