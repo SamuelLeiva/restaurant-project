eec2374ddc659273df0c5870ed8ba4fa0a13004b
SamuelLeiva <leiva20152@gmail.com>
2025-04-28 10:12:45 -0500

    Clase abstracta persona modelada

---
+++ RestaurantProject/Models/Abstracts/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestaurantProject.Models.Enums;
+
+namespace RestaurantProject.Models.Abstracts;
+
+public abstract class Person
+{
+    public string Name { get; set; }
+    public Genre Genre { get; set; }
+    public string Dni { get; set; }
+    public string Adress { get; set; }
+    public int Age { get; set; }
+}
