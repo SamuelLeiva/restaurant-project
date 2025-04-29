7bc047b3b40cf029b08d781b9c0457970870f792
SamuelLeiva <leiva20152@gmail.com>
2025-04-29 10:20:20 -0500

    Método FillClients modelado

--- RestaurantProject/Services/ClientService.cs
+++ RestaurantProject/Services/ClientService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using RestaurantProject.Models;
 using RestaurantProject.Models.Enums;
 using RestaurantProject.Models.Interfaces;
@@ -83,6 +85,25 @@ public class ClientService : IClientService
 
     public void FillData()
     {
+        var client1 = new Client()
+        {
+            Name = "Samuel Leiva",
+            Genre = Genre.MASCULINO,
+            Dni = "78874956",
+            Address = "Av La Marina 567",
+            Age = 35
+        };
 
+        var client2 = new Client()
+        {
+            Name = "Juana Perez",
+            Genre = Genre.FEMENINO,
+            Dni = "65559948",
+            Address = "Av Izaguirre 456",
+            Age = 27
+        };
+
+        clientsDB.Add(client1);
+        clientsDB.Add(client2);
     }
 }
